dba3d91a94225c30b946bd3246ca0f8017a9f9d9
Tharun Sai Vaddi <tharun.vaddi2k@gmail.com>
2024-09-30 13:36:46 -0700

    Backend implementation of user authentication

--- backend/server.js
+++ backend/server.js
@@ -2,10 +2,29 @@ const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 5001;
 
-app.get('/', (req, res) => {
-    res.send('Backend is running!');
-});
+const connectDB = require('./config/db');
+const cors = require('cors');
+const authRoutes = require('./routes/auth');
+
+
+connectDB();
+
+
+app.use(express.json());
+app.use(cors());
+
+
+app.use('/api/auth', authRoutes);
 
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
 });
+
+
+
+
+
+
+
+
+
