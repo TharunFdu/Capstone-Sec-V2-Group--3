dbd75296c0fdfa9348a962704f04060f7eb6ce14
Tharun Sai Vaddi <tharun.vaddi2k@gmail.com>
2024-10-06 18:49:29 -0700

    Updated middleware

--- backend/middleware/authMiddleware.js
+++ backend/middleware/authMiddleware.js
@@ -1,17 +1,25 @@
 const jwt = require('jsonwebtoken');
 const keys = require('../config/keys');  
 
-module.exports = (req, res, next) => {
+module.exports.auth = (req, res, next) => {
     const token = req.header('Authorization');
     if (!token) {
         return res.status(401).json({ message: 'No token, authorization denied' });
     }
 
     try {
-        const decoded = jwt.verify(token, keys.jwtSecret);
-        req.user = decoded;
-        next();
+        const decoded = jwt.verify(token.split(" ")[1], keys.jwtSecret);  
+        req.user = decoded;  
+        next();  
     } catch (err) {
-        res.status(401).json({ message: 'Token is not valid' });
+        res.status(401).json({ message: 'Invalid token' });
     }
+};
+
+module.exports.isAdmin = (req, res, next) => {
+  if (req.user && req.user.role === 'admin') {
+    next();
+  } else {
+    return res.status(403).json({ message: 'Access denied. Admins only.' });
+  }
 };
