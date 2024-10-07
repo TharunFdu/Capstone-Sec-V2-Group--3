29003dd0b1cfe189fb0eb87bbf9ff689253a28dc
Tharun Sai Vaddi <tharun.vaddi2k@gmail.com>
2024-10-06 18:51:44 -0700

    Updated server.js

--- backend/server.js
+++ backend/server.js
@@ -4,6 +4,9 @@ const PORT = process.env.PORT || 5001;
 const sequelize = require('./config/db');  
 const cors = require('cors');
 const authRoutes = require('./routes/auth');
+const eventRoutes = require('./routes/eventRoutes');
+const venueRoutes = require('./routes/venueRoutes');
+const bookingRoutes = require('./routes/bookingRoutes');
 
 
 app.use(express.json());
@@ -15,7 +18,13 @@ sequelize.sync()
 
 app.use('/api/auth', authRoutes);
 
+app.use('/api/events', eventRoutes);
+app.use('/api/venues', venueRoutes);
+
+app.use('/api/bookings', bookingRoutes);
+
+
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
 });
- 
+
